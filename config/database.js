4ec111cba1f3115a9c449ec9e96be48226cdbb6b
Cyprien Siaud <74303569+CSIAUD@users.noreply.github.com>
2022-05-18 14:11:17 +0200

    add mateo

--- config/database.js
+++ config/database.js
@@ -3,11 +3,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-<<<<<<< HEAD
-      port: env.int('DATABASE_PORT', 5432),
-=======
       port: env.int('DATABASE_PORT', 5434),
->>>>>>> f3b22dcf1305886da1aa62f9e1cb28b7a7ce5ec6
       database: env('DATABASE_NAME', 'ecuries-persevere'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'root'),
